f498bac48309a4733e01c4f6914868b0d1f0cdcd
Gojohime <kadrishaban999@gmail.com>
2024-05-28 15:02:31 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "srv1339.hstgr.io",
+  user: "u242778090_kadrishaban55",
+  password: "Nilofer567",
+  database: "u242778090_kadrishaban55",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hello this is backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+  const values = [
+    "title from backend",
+    "desc from backend",
+    "cover pic from backend",
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book has been created successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to Backend!");
+});
---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+  return (
+    <div>
+      <h1>Shaban Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add</Link>
+      </button>
+    </div>
+  );
+};
+
+export default Books;
